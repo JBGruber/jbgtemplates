a910aa1d2538e7f238c3da56e4bb89aebd5f40da
JBGruber <johannesb.gruber@gmail.com>
2025-09-09 10:39:44 +0200

    improve install_deps

--- R/install_deps.r
+++ R/install_deps.r
@@ -13,21 +13,38 @@
 #' install_deps(gh_packages = "JBGruber/paperboy")
 #' }
 install_deps <- function(path = ".",
+                         pattern = "*.[.](Rmd|rmd|qmd)$",
+                         recursive = TRUE,
                          packages = NULL,
-                         gh_packages = NULL) {
+                         gh_packages = NULL,
+                         dependencies = TRUE) {
   rlang::check_installed("attachment", "in order to use this function")
-  files <- list.files(path, pattern = ".R$|.Rmd$|.Qmd", full.names = TRUE,
-                      recursive = TRUE, ignore.case = TRUE)
+  files <- list.files(
+    path = path,
+    full.names = TRUE,
+    recursive = TRUE,
+    ignore.case = TRUE
+  )
+  # the pattern matching of list.files is not great
+  files <- grep(pattern = pattern, x = files, value = TRUE, perl = TRUE)
   files <- split(files, tolower(tools::file_ext(files)))
 
-  needed_packages <- c(
+  needed_packages <- unique(c(
     if (!is.null(files$rmd)) attachment::att_from_rmds(files$rmd),
     if (!is.null(files$qmd)) attachment::att_from_qmds(files$qmd),
     if (!is.null(files$r))  attachment::att_from_rscripts(files$r),
     packages
-  )
-  rlang::check_installed(unique(needed_packages))
-  for (p in gh_packages) install_deps_gh(p)
+  ))
+
+  if (rlang::is_installed("pak")) {
+    action <- function(pkgs, ...) pak::pak(pkgs, dependencies = dependencies)
+    if (!is.null(gh_packages)) action(gh_packages)
+  } else {
+    action <- function(pkgs, ...) install.packages(pkgs, dependencies = dependencies)
+    for (p in gh_packages) install_deps_gh(p)
+  }
+
+  rlang::check_installed(unique(needed_packages), action = action)
 }
 
 install_deps_gh <- function(package) {
